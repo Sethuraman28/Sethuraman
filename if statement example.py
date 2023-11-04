a2b80e17ea6307035cbf336366bfe5842935956b
Sethuraman28 <sethuraman210181@gmail.com>
2023-11-05 00:09:17 +0530

    Update if statement example.py

--- if statement example.py
+++ if statement example.py
@@ -1,7 +1,15 @@
-age=int(input("Enter your age:"))
-if age<18:
-    print("You are a minor")
-elif age<65:
-    print("You are an adult")
-else:
-    print("You are a senior")
+def vote():
+    age=int(input("Enter your age:"))
+    if age>0 & age<18:
+        print("You are a minor")
+        vote()
+    elif age<0:
+        print("Enter valid number")
+        vote()
+    elif age<65:
+        print("You are an adult")
+        vote()
+    elif age>=65:
+        print("You are a senior")
+        vote()
+vote()
