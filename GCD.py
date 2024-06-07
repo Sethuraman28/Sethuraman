d4485037583306e99a94baa99d27a595bc5dcec6
Sethuraman28 <129416261+Sethuraman28@users.noreply.github.com>
2024-06-07 21:46:29 +0530

    Delete GCD.py

--- GCD.py
+++
@@ -1,7 +0,0 @@
-def gcd():
-	import math
-	print("\t\tGCD CALCULATOR")
-	a=int(input("Enter a number:"))
-	b=int(input("Enter another number:"))
-	print("The GCD of the entered numbers is ",math.gcd(a,b))
-gcd()
