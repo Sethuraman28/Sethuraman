64b09bce932d699c92d6c477c6666a4c79e54588
Sethuraman28 <129416261+Sethuraman28@users.noreply.github.com>
2024-06-07 21:42:37 +0530

    Delete Chr Replace In Str.py

--- Chr Replace In Str.py
+++
@@ -1,8 +0,0 @@
-str=input("Enter a string:")
-a=int(input("If the string you entered is okay press 1 or if you want to change any character press 0:"))
-if a==0:
-	x=input("Enter the character you want to change:")
-	y=input("Enter new character:")
-	print(str.replace(x,y))
-else:
-	print(str)
