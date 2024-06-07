a5079db9fecdcdd502c0ddc30a348f31d7409a43
Sethuraman28 <129416261+Sethuraman28@users.noreply.github.com>
2024-06-07 21:42:25 +0530

    Delete Celsius to Fahrenheit.py

--- Celsius to Fahrenheit.py
+++
@@ -1,7 +0,0 @@
-print("------------------------CELSIUS TO FAHRENHEIT----------------------")
-def cel_fah():
-	celsius = float(input("\nEnter the celsius value:"))
-	fahrenheit = (celsius * 1.8) + 32
-	print("{0} degree Celsius is equal to {1} degree Fahrenheit".format(celsius,fahrenheit))
-	cel_fah()
-cel_fah()
