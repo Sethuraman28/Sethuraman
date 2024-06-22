2f29442b6daafb46cdb9d7ead904c409de8864f0
Sethuraman28 <sethuraman210181@gmail.com>
2024-06-22 15:12:36 +0530

    python files

--- multiplication table.py
+++
@@ -1,8 +0,0 @@
-print("------------------------MULTIPLICATION TABLE-----------------------")
-def multip_table():
-	n=int(input("\nDisplay multiplication table of which number:"))
-	print("Here is the multiplication table of {}..............".format(n))
-	for num in range(0,21):
-		print(n,"x",num,"=",n*num)
-	multip_table()
-multip_table()
